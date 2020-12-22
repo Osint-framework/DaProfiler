2b06b34eda47e552a66d464625043447ae93f779
Dalunacrobate <76447679+dalunacrobate@users.noreply.github.com>
2020-12-22 05:44:05 +0100

    Update mail_gen.py

--- modules/mail_gen.py
+++ modules/mail_gen.py
@@ -1,15 +1,23 @@
 import threading, requests, bs4
 from bs4 import BeautifulSoup
-from modules import gmail_check
+from modules import mail_check
 
 def check(name,pren):
     results = [
         "{}.{}@gmail.com".format(name,pren),
-        "{}{}@gmail.com".format(name,pren)
+        "{}{}@gmail.com".format(name,pren),
+        "{}.{}@yahoo.com".format(name,pren),
+        "{}{}@yahoo.com".format(name,pren),
+        "{}.{}@aol.com".format(name,pren),
+        "{}{}@aol.com".format(name,pren),
+        "{}.{}@hotmail.com".format(name,pren),
+        "{}{}@hotmail.com".format(name,pren),
+        "{}.{}@outlook.com".format(name,pren),
+        "{}{}@outlook.com".format(name,pren),    
     ]
     valid_mails = []
     for i in results:
-        a = gmail_check.verify(mail=i)
+        a = mail_check.verify(mail=i)
         if a is not None:
             valid_mails.append(i)
     return valid_mails
@@ -29,17 +37,22 @@ def skype2email(name,pren):
         if "live:." in i.text:
             pass
         else:
-            profiless.append(i.text.replace('live:',''))
+            profiless.append(i.text.replace('live:','').replace('_1',''))
 
     valid_emails = []
 
     for i in profiless:
-        if "_1" in i:
-            i.replace('_1','')
-        email = i+"@gmail.com"
-        a = gmail_check.verify(mail=email)
-        if a is not None:
-            valid_emails.append(email)
+        emails = [
+            i+"@aol.com",
+            i+"@yahoo.com",
+            i+"@gmail.com",
+            i+"@hotmail.com",
+            i+"@outlook.com"
+        ]
+        for i in emails:
+            a = mail_check.verify(mail=i)
+            if a is not None:
+                valid_emails.append(i)
     return valid_emails
 
 # By Daluna#1313 from Prism Intelligence Group
