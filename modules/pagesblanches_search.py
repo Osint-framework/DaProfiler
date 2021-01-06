0f87999459118ceed84caba7864a2c7dea6f8986
Dalunacrobate <76447679+dalunacrobate@users.noreply.github.com>
2021-01-06 23:59:48 +0100

    Added phone location and carrier

--- modules/pagesblanches_search.py
+++ modules/pagesblanches_search.py
@@ -18,28 +18,12 @@ def adresse_search(name,pren):
         phon_full = (target_phon.text.strip())
 
         if name.lower() in name_full.lower():
-            prefix = (phon_full[0:2])
-            if prefix == "06" or prefix == "07":
-                type_tel = ("Portable")
-            elif prefix == "08" or prefix == "09":
-                type_tel = ("Voip/FAI")
-            else:
-                type_tel = ("Fixe")
-            localisation = None
-            if type_tel != "Fixe" and type_tel != "Voip/FAI":
-                if prefix == "01":
-                    localisation = "Ile De France"
-                elif prefix == "02":
-                    localisation = "Nord-Ouest de la France"
-                elif prefix == "03":
-                    localisation = "Nord-Est de la France"
-                elif prefix == "04":
-                    localisation = "Sud-Est de la France"
-                elif prefix == "05":
-                    localisation = "Sud-Ouest de la France."
-                else:
-                    localisation = "Introuvable"
-            text = {"Name":name_full,'Phone':phon_full,'Adress':addr_full,'Loc_phone':localisation,'Type_tel':type_tel}
+            prefix = (phon_full[1:])
+            full_phone = (prefix.replace(' ',''))
+            url = "http://apilayer.net/api/validate?access_key=4395fd642c2f1b24f5d9d01e0a1f838a&number=+33{}&country_code=&format=1".format(full_phone)
+            r = requests.get(url)
+            data = r.json()
+            text = {'Phone':phon_full,'Name':name_full,'Adress':addr_full,'Type_tel':data['line_type'],"Loc_phone":data['country_name']+data['location'],'carrier':data['carrier']}
             return text
         else:
             return None
