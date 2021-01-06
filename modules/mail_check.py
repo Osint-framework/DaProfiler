451dd0f17a5e52847b5b5e7f0bf5df2745cb58e7
Dalunacrobate <76447679+dalunacrobate@users.noreply.github.com>
2021-01-06 23:11:22 +0100

    Update discord username

--- modules/mail_check.py
+++ modules/mail_check.py
@@ -4,12 +4,16 @@ import requests
 
 def verify(mail):
     response = requests.get("https://isitarealemail.com/api/email/validate?email={}".format(mail),params = {'Authorization':'fa86a707-750e-485c-8ec3-86eddd7ec4d0'},headers = {'Authorization': "Bearer fa86a707-750e-485c-8ec3-86eddd7ec4d0"})
+    try:
+        data = response.json()
+        status = data['status']
+        if status == "valid":
+            return True
+        elif status == "invalid":
+            return None
+        else:
+            return None
+    except:
+        return None
 
-    data = response.json()
-    status = data['status']
-    if status == "valid":
-        return True
-    elif status == "invalid":
-        return None
-    else:
-        return None
+# By Lui#6166 from Prism Intelligence Group
