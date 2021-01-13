c7a0e92e26f2858d3c20fd5db43d32f6d6dc39f3
Dalunacrobate <76447679+dalunacrobate@users.noreply.github.com>
2021-01-13 14:50:46 +0100

    Actual time printed

--- modules/visual/logging.py
+++ modules/visual/logging.py
@@ -1,7 +1,11 @@
+from datetime import datetime
 import time
 
 def terminal_loggin(log,text):
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    text = (current_time, text)
     if log == "True":
         for i in text: 
             print(i, end ='', flush = True) 
-            time.sleep(0.01) 
+            time.sleep(0.01)
